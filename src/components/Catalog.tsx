4465a2ee5aee2494f3db7b6704f1707c51fe5d64
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-09-23 21:46:59 -0300

    Configurando Store

---
+++ src/components/Catalog.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const Catalog: React.FC = () => {
+  const catalog = useSelector((state) => state);
+
+  console.log(catalog);
+
+  return <h1>Hello world</h1>;
+};
+
+export default Catalog;

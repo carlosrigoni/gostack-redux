796e39e7babbe55f3a8d846bd669bcd4b7746067
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-09-24 11:11:12 -0300

    Criando Action Types

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -7,6 +7,7 @@ import {
 import { IState } from '../../';
 import { AxiosResponse } from 'axios';
 import api from '../../../services/api';
+import { ActionTypes } from './types';
 
 type checkProductStockRequest = ReturnType<typeof addProductToCartRequest>;
 
@@ -38,5 +39,5 @@ function* checkProductStock({ payload }: checkProductStockRequest) {
 }
 
 export default all([
-  takeLatest('ADD_PRODUCT_TO_CART_REQUEST', checkProductStock),
+  takeLatest(ActionTypes.addProductToCartRequest, checkProductStock),
 ]);

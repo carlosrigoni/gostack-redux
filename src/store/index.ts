2df09a2be2b07dc9c6b49d77d3bdeb7e1ca6e843
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-09-23 22:06:57 -0300

    Usando os reducers

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import { createStore } from 'redux';
+import rootReducer from './modules/rootReducers';
 
-const store = createStore(() => []);
+const store = createStore(rootReducer);
 
 export default store;

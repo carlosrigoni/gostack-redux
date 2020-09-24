605cdcbcbbae45ad1969d430f9a8491daab358cb
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-09-24 11:05:07 -0300

    Sinalizando falta de estoque

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { IState } from '../store';
 import { addProductToCartRequest } from '../store/modules/cart/actions';
 import { IProduct } from '../store/modules/cart/types';
 
@@ -9,6 +10,10 @@ interface CatalogItemProps {
 
 const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
   const dispatch = useDispatch();
+
+  const hasFailedStockCheck = useSelector<IState, boolean>((state) => {
+    return state.cart.failedStockCheck.includes(product.id);
+  });
 
   const handleAddProductToCart = useCallback(() => {
     dispatch(addProductToCartRequest(product));
@@ -21,6 +26,7 @@ const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
       <button type="button" onClick={handleAddProductToCart}>
         Comprar
       </button>
+      {hasFailedStockCheck && <span style={{ color: 'red' }}>Sem estoque</span>}
     </article>
   );
 };
--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -5,6 +5,7 @@ import { ICartState } from './types';
 
 const INITIAL_STATE: ICartState = {
   items: [],
+  failedStockCheck: [],
 };
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
@@ -29,7 +30,8 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
         break;
       }
       case 'ADD_PRODUCT_TO_CART_FAILURE': {
-        console.log('failure', action.payload);
+        draft.failedStockCheck.push(action.payload.productId);
+        break;
       }
       default: {
         return state;
